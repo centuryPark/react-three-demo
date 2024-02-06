bdc3869af196037fb0d4b39479714b8260b05147
gongyuan <parkgong@tencent.com>
2024-02-06 17:33:55 +0800

    feat: model page

--- src/app/components/box.tsx
+++ src/app/components/box.tsx
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import React, { useRef, useState } from 'react';
 import { useFrame } from '@react-three/fiber'
 
-const BoxScale = ({ position } : any) => {
+const BoxScale = ({ position, size = [1, 1, 1] } : any) => {
   const boxRef = useRef<THREE.Mesh>(null!);
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
@@ -12,6 +12,7 @@ const BoxScale = ({ position } : any) => {
   return (
     <mesh
       ref={boxRef}
+      castShadow
       position={position}
       scale={active ? 1.2 : 1}
       onClick={() => setActive(!active)}
@@ -19,7 +20,7 @@ const BoxScale = ({ position } : any) => {
       onPointerOut={() => setHover(true)}
     >
       <boxGeometry
-        args={[1, 1, 1]}
+        args={size}
       />
       <meshStandardMaterial color={hovered ? 'yellow' : 'orange'} />
     </mesh>
--- src/app/model/page.tsx
+++ src/app/model/page.tsx
@@ -1,15 +1,88 @@
 'use client';
 
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useLoader, useFrame } from '@react-three/fiber';
 import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
 import styles from "../page.module.css";
+import { useEffect, useRef } from 'react';
+import { PerspectiveCamera, Stats, OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
+
+/* 
+camera={{
+          position: [100, 200, 300],
+          fov: 45, // 摄像机视锥体垂直视野角度
+          aspect: window.innerWidth / window.innerHeight, // 摄像机视锥体长宽比
+          near: 1, //摄像机视锥体近端面
+          far: 2000, //摄像机视锥体远端面
+
+        }}
+*/
+
+const Modal = () => {
+  const groupRef = useRef<any>(null!);
+  const fbx = useLoader(FBXLoader, '/models/Samba Dancing.fbx');
+  let mixer: any;
+  if (fbx.animations.length) {
+    mixer = new THREE.AnimationMixer(fbx);
+    const action = mixer.clipAction(fbx.animations[0]);
+    action.play();
+  }
+
+  fbx.traverse(function (child: any) {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+
+  useFrame((state, delta) => {
+    mixer?.update(delta)
+  })
+
+  return (
+    <primitive ref={groupRef} object={fbx} position={[10, 0, 0]} />
+  )
+};
 
 export default function Home() {
+  const containerRef = useRef<HTMLDivElement>(null!);
+
   return (
-    <main className={styles.main}>
-      <Canvas>
-        <ambientLight color="#fff" intensity={1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
+    <main ref={containerRef} className={styles.main}>
+      <Canvas shadows>
+        <OrbitControls target={[0, 100, 0]} />
+        <PerspectiveCamera
+          position={[100, 200, 500]}
+          fov={45}
+          aspect={window.innerWidth / window.innerHeight}
+          near={1}
+          far={1000}
+          castShadow
+          makeDefault />
+        <color attach="background" args={['#a0a0a0']} />
+        <fog attach="fog" color="#a0a0a0" near={200} far={1000} />
+        <hemisphereLight position={[0, 200, 0]} args={['#ffffff', '#444', 5]} />
+        <directionalLight
+          args={['#fff', 5]}
+          castShadow
+          position={[0, 200, 100]}>
+          <orthographicCamera attach="shadow-camera" args={[-100, 150, 300, -200]} />
+        </directionalLight>
+        <mesh
+          receiveShadow
+          rotation={[- Math.PI / 2, 0, 0]}
+          position={[0, 0, 0]}
+        >
+          <planeGeometry attach="geometry" args={[20000, 20000]} />
+          <meshStandardMaterial attach="material" color="#000" depthWrite={false} />
+        </mesh>
+        <gridHelper
+          position={[0, 0, 0]}
+          args={[2000, 20, 0x000000, 0x000000]}
+        >
+        </gridHelper>
+        <Stats />
+        <Modal />
       </Canvas>
     </main>
   );
